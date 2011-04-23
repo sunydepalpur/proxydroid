3d4146fb9c48a2a8253557114dadbbe957fca248
Max <Max@localhost>
2011-04-23 18:41:30 +0800

    begin from here

---
+++ src/org/proxydroid/ProxyDroidReceiver.java
@@ -0,0 +1,55 @@
+package org.proxydroid;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class ProxyDroidReceiver extends BroadcastReceiver {
+
+	private String host;
+	private String proxyType;
+	private int port;
+	private String user;
+	private String password;
+	private boolean isAutoStart = false;
+	private boolean isAuth = false;
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		SharedPreferences settings = PreferenceManager
+				.getDefaultSharedPreferences(context);
+
+		isAutoStart = settings.getBoolean("isAutoStart", false);
+
+		if (isAutoStart) {
+			host = settings.getString("host", "");
+			proxyType = settings.getString("proxyType", "http");
+			user = settings.getString("user", "");
+			password = settings.getString("password", "");
+			isAuth = settings.getBoolean("isAuth", false);
+			String portText = settings.getString("port", "");
+			try {
+				port = Integer.valueOf(portText);
+			} catch (Exception e) {
+				port = 3128;
+			}
+
+			Intent it = new Intent(context, ProxyDroidService.class);
+			Bundle bundle = new Bundle();
+			bundle.putString("host", host);
+			bundle.putString("proxyType", proxyType);
+			bundle.putInt("port", port);
+			bundle.putString("user", user);
+			bundle.putString("password", password);
+			bundle.putBoolean("isAuth", isAuth);
+
+			it.putExtras(bundle);
+			context.startService(it);
+		}
+	}
+
+}
