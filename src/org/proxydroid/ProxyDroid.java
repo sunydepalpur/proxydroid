b6253c0c68715d6733b625c22107a2dde2ad6605
max c lv <max.c.lv@gmail.com>
2011-05-17 18:44:13 +0800

    busybox embedded

--- src/org/proxydroid/ProxyDroid.java
+++ src/org/proxydroid/ProxyDroid.java
@@ -354,6 +354,7 @@ public class ProxyDroid extends PreferenceActivity implements
 			runCommand("chmod 777 /data/data/org.proxydroid/redsocks");
 			runCommand("chmod 777 /data/data/org.proxydroid/proxy.sh");
 			runCommand("chmod 777 /data/data/org.proxydroid/cntlm");
+			runCommand("chmod 777 /data/data/org.proxydroid/busybox");
 			edit = settings.edit();
 			edit.putBoolean(versionName, true);
 			edit.commit();
