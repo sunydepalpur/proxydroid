8472e3c5759e656cba62480f58fcd105c1b4b450
max.c.lv <max.c.lv@localhost>
2011-08-03 08:34:49 +0800

    fix

--- src/org/proxydroid/ConnectivityBroadcastReceiver.java
+++ src/org/proxydroid/ConnectivityBroadcastReceiver.java
@@ -150,7 +150,6 @@ public class ConnectivityBroadcastReceiver extends BroadcastReceiver {
 			}
 		}
 
-		mProfile.getProfile(settings);
 		if (curSSID != null) {
 			if (!Utils.isWorked(context)) {
 				ProxyDroidReceiver pdr = new ProxyDroidReceiver();
