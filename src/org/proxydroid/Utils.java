0ade6015c5fb230135b076d20b2adbb85004759e
max.c.lv <max.c.lv@localhost>
2011-12-28 13:00:04 +0800

    preserve

--- src/org/proxydroid/Utils.java
+++ src/org/proxydroid/Utils.java
@@ -37,8 +37,8 @@ class Utils {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
-			if (c == '\'')
-				sb.append(c);
+			if (c == '\\' || c == '$' || c == '`' || c == '"')
+				sb.append('\\');
 			sb.append(c);
 		}
 		return sb.toString();
