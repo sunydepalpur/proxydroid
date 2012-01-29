1f42ca76111578b192f7b22f3797e352dfac8fad
Max <Max@localhost>
2012-01-29 12:38:20 +0800

    update for auto connect function

---
+++ src/org/proxydroid/Constraints.java
@@ -0,0 +1,9 @@
+package org.proxydroid;
+
+public class Constraints {
+
+    public static final String ONLY_3G = "2G/3G";
+    public static final String ONLY_WIFI = "WIFI";
+    public static final String WIFI_AND_3G = "WIFI/2G/3G";
+
+}
